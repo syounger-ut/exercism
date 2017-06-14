da8baec33d71a00c4f1aa6ef94f6a8d5b1fb4706
samyounger <sam.younger@gmail.com>
2017-06-14 21:46:56 +0100

    Difference of squares problem submitted

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,10 +1,27 @@
+module BookKeeping
+  VERSION = 3
+end
+
 class Squares
 
-  def self.square_of_sum(number)
-    ten_numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+  def initialize(number)
+    @numbers = numbers_array(number)
+  end
 
-    ten_numbers.each { |n|  }
+  def square_of_sum
+    @numbers.inject(:+) ** 2 rescue 0
+  end
 
+  def sum_of_squares
+    @numbers.map { |n| n ** 2 }.inject(:+) rescue 0
+  end
+
+  def difference
+    square_of_sum - sum_of_squares rescue 0
+  end
+
+  def numbers_array(number)
+    number.times.map { |n| n + 1 }
   end
 
 end
