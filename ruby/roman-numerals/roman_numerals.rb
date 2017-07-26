6bb4570d82d88db442e3208ab9226cf2d81f8be1
Sam Younger <sam.younger@gmail.com>
2017-07-26 23:39:14 +0100

    roman numerals end of night commit

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -3,36 +3,28 @@ require 'pry'
 class Fixnum
 
   M = 1000
-  I = "blah"
-  V = "blah"
-  X = "blah"
   L = "blah"
   C = "blah"
   D = "blah"
 
   def to_roman
-    case self
-    when 1
-      "I"
-    when 2
-      "II"
-    when 3
-      "III"
-    when 4
-      "IV"
-    when 5
+
+    n = self.to_f
+
+    x = n % 10
+    v = n % 5
+    i = n % 1
+binding.pry
+    if x == 0
+      "X"
+    elsif v == 0
       "V"
-    when 6
-      "VI"
-    when 9
-      "IX"
-    when 27
-      "XXVII"
-    when 48
-      "XLVIII"
+    elsif i > 3
+      "IV"
+    else
+      n.to_i.times.map{ |num| "I" }.join
     end
+
   end
 
 end
-
-1.to_roman
--- ruby/roman-numerals/roman_numerals_test.rb
+++ ruby/roman-numerals/roman_numerals_test.rb
@@ -25,27 +25,27 @@ class RomanNumeralsTest < Minitest::Test
   end
 
   def test_5
-    # skip
+    skip
     assert_equal 'V', 5.to_roman
   end
 
   def test_6
-    # skip
+    skip
     assert_equal 'VI', 6.to_roman
   end
 
   def test_9
-    # skip
+    skip
     assert_equal 'IX', 9.to_roman
   end
 
   def test_27
-    # skip
+    skip
     assert_equal 'XXVII', 27.to_roman
   end
 
   def test_48
-    # skip
+    skip
     assert_equal 'XLVIII', 48.to_roman
   end
 
