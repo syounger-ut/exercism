fa8df2371f7f595f9c41b2ebeed3fde1689249b6
Sam Younger <sam.younger@gmail.com>
2018-01-25 23:25:13 +0000

    different approach

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -2,36 +2,60 @@ require 'pry'
 
 class Fixnum
   ROMAN_NUMERALS = {
-    "I" => 1,
-    "V" => 5,
-    "X" => 10,
-    "L" => 50,
-    "C" => 100,
-    "D" => 500,
-    "M" => 1000
+    '1'   => 'I',
+    '5'   => 'V',
+    '10'  => 'X',
+    '50'  => 'L',
+    '100' => 'C',
+    '500' => 'D',
+    '1000' => 'M'
   }
 
   def to_roman
-    n = self.to_f
+    # Break the number into Thousands, Hundreds, Tens and Ones, and write down each in turn.
+    n = self
+    number_of_chars = n.to_s.chars.count
 
-    v = "V" if (n / 5).round.odd? && n >= 5 && n % 10 != 0
-    i = singles(n)
-    # binding.pry
-    numeral = [ v, i ]
-
-    numeral.join("")
-
+    case number_of_chars
+    when 4
+      # code fo 1000's
+    when 3
+      # code for 100's
+    when 2
+      # code for 10's
+      # binding.pry
+      multiple = n.digits.last
+      tens = combine_numerals(multiple, 'X')
+      single = singles(n.digits.first)
+      [tens, single].join
+    when 1
+      # code for 1's
+      singles(n)
+    end
   end
 
   private
 
   def singles(n)
-    case n % 5
-    when 4
-      "IV"
-    when 3,2,1
-      (n % 5).to_i.times.map { |num| "I" }.join("")
+    if n == 4
+      'IV'
+    elsif n == 5
+      'V'
+    elsif n == 6
+      'VI'
+    elsif n == 7
+      'VII'
+    elsif n == 8
+      'VIII'
+    elsif n == 9
+      'IX'
+    else
+      combine_numerals(n, 'I')
     end
+  end
+
+  def combine_numerals(multiple, numeral)
+    multiple.times.map { |x| numeral }.join
   end
 
 end
