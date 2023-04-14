a9bdcfeda9588ef46d6181326c0f0ad9fbae0732
samyounger <sam.younger@gmail.com>
2023-04-15 00:07:43 +0100

    bracket matching opposing pairs

--- scala/matching-brackets/src/main/scala/MatchingBrackets.scala
+++ scala/matching-brackets/src/main/scala/MatchingBrackets.scala
@@ -1,9 +1,31 @@
+import scala.annotation.tailrec
 import scala.util.matching.Regex
 import scala.collection.mutable.ListBuffer
 
 
 object MatchingBrackets {
   def isPaired(brackets : String) : Boolean = {
+    // Early return when an empty string
+    if (brackets.isEmpty) return true
 
+    val chars = brackets
+      .filterNot(_.isWhitespace) // Remove white space
+      .toList
+
+    def matchPair(startChar: Char, endChar: Char): Boolean = {
+      startChar match {
+        case '[' => endChar == ']'
+        case '{' => endChar == '}'
+        case _ => false
+      }
+    }
+
+    @tailrec
+    def matchPairs(n: Int = 0, aggregator: Boolean = true): Boolean = {
+      if (!aggregator || n > (chars.length / 2) - 1) aggregator
+      else matchPairs(n + 1, matchPair(chars(n), chars(chars.length - 1 - n)))
+    }
+
+    matchPairs()
   }
 }
