b3a3ab077d9629dda990a5dbcc1bd3b577add752
samyounger <sam.younger@gmail.com>
2023-11-22 23:45:09 +0000

    Leap year solution

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,3 +1,17 @@
 object Leap {
-  def leapYear(year: Int): Boolean = ???
+  private val COMMON_LEAP_YEAR_CADENCE = 4
+  private val CENTURY_LEAP_YEAR = 100
+
+  def leapYear(year: Int): Boolean = {
+    isLeapYearCandidate(year) && !leapYearException(year)
+  }
+
+  private def isLeapYearCandidate(year: Int): Boolean = {
+    year % COMMON_LEAP_YEAR_CADENCE == 0
+  }
+
+  private def leapYearException(year: Int): Boolean = {
+    (year % CENTURY_LEAP_YEAR == 0) &&
+      !(year % (CENTURY_LEAP_YEAR * COMMON_LEAP_YEAR_CADENCE) == 0)
+  }
 }
