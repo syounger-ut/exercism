b9c6bdab2145b30a3d6d0b918bbac77b5ca8a9c7
samyounger <sam.younger@gmail.com>
2023-11-22 23:02:25 +0000

    Working solution

--- scala/matching-brackets/src/main/scala/MatchingBrackets.scala
+++ scala/matching-brackets/src/main/scala/MatchingBrackets.scala
@@ -1,31 +1,56 @@
 import scala.annotation.tailrec
-import scala.util.matching.Regex
-import scala.collection.mutable.ListBuffer
-
 
 object MatchingBrackets {
-  def isPaired(brackets : String) : Boolean = {
-    // Early return when an empty string
-    if (brackets.isEmpty) return true
+  val open_chars: Array[Char] = Array('[', '{', '(')
+  val close_chars: Array[Char] = Array(']', '}', ')')
 
-    val chars = brackets
-      .filterNot(_.isWhitespace) // Remove white space
-      .toList
+  def isPaired(brackets: String, stack: List[Char] = List()): Boolean = {
+    // What if the brackets is empty?
+    if (brackets.isEmpty) {
+      stack.isEmpty
+    // What if the brackets has characters?
+    } else {
+      bracket_has_characters(brackets, stack)
+    }
+  }
 
-    def matchPair(startChar: Char, endChar: Char): Boolean = {
-      startChar match {
-        case '[' => endChar == ']'
-        case '{' => endChar == '}'
-        case _ => false
+  private def bracket_has_characters(brackets: String, stack: List[Char]): Boolean = {
+    // Get the first character
+    val bracket = brackets.head
+    // Check of the character is an open character
+    if (open_chars.contains(bracket)) {
+      // Add the character to the stack
+      isPaired(brackets.tail, bracket :: stack)
+    } else {
+      not_open_character(bracket, brackets, stack)
+    }
+  }
+
+  private def not_open_character(bracket: Char, brackets: String, stack: List[Char]): Boolean = {
+    // Check if the character is a close character
+    if (close_chars.contains(bracket)) {
+      closing_character(bracket, brackets, stack)
+    } else {
+      // If the character is not an open character, we ignore it
+      isPaired(brackets.tail, stack)
+    }
+  }
+
+  private def closing_character(bracket: Char, brackets: String, stack: List[Char]): Boolean = {
+    // Check if the stack is empty
+    if (stack.isEmpty) {
+      false
+    } else {
+      // Get the index of the closing character
+      val current_close_char_index = close_chars.indexOf(bracket)
+      // Get the character at position n
+      val matchedOpenChar = open_chars(current_close_char_index)
+      if (matchedOpenChar == stack.head) {
+        // Remove the last character from the stack
+        isPaired(brackets.tail, stack.tail)
+      } else {
+        false
       }
     }
-
-    @tailrec
-    def matchPairs(n: Int = 0, aggregator: Boolean = true): Boolean = {
-      if (!aggregator || n > (chars.length / 2) - 1) aggregator
-      else matchPairs(n + 1, matchPair(chars(n), chars(chars.length - 1 - n)))
-    }
-
-    matchPairs()
   }
 }
--- scala/matching-brackets/src/test/scala/MatchingBracketsTest.scala
+++ scala/matching-brackets/src/test/scala/MatchingBracketsTest.scala
@@ -36,37 +36,30 @@ class MatchingBracketsTest extends FunSuite with Matchers {
   }
 
   test("several paired brackets") {
-    pending
     MatchingBrackets.isPaired("{}[]") should be(true)
   }
 
   test("paired and nested brackets") {
-    pending
     MatchingBrackets.isPaired("([{}({}[])])") should be(true)
   }
 
   test("unopened closing brackets") {
-    pending
     MatchingBrackets.isPaired("{[)][]}") should be(false)
   }
 
   test("unpaired and nested brackets") {
-    pending
     MatchingBrackets.isPaired("([{])") should be(false)
   }
 
   test("paired and wrong nested brackets") {
-    pending
     MatchingBrackets.isPaired("[({]})") should be(false)
   }
 
   test("math expression") {
-    pending
     MatchingBrackets.isPaired("(((185 + 223.85) * 15) - 543)/2") should be(true)
   }
 
   test("complex latex expression") {
-    pending
     MatchingBrackets.isPaired(
       """\left(\begin{array}{cc} \frac{1}{3} & x\ \mathrm{e}^{x} &... x^2 \end{array}\right)""") should be(
       true)
