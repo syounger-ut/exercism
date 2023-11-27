ba22509320fa68fd135be0fe9cae816ee02e891d
Sam Younger <syounger@usertesting.com>
2023-11-27 23:28:15 +0000

    Working solution

--- scala/grade-school/src/main/scala/GradeSchool.scala
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -1,12 +1,36 @@
 class School {
   type DB = Map[Int, Seq[String]]
+  private var database: DB = Map()
 
-  def add(name: String, g: Int) = ???
+  def add(name: String, g: Int): DB = {
+    val studentNames = {
+      if (!db.contains(g)) Seq()
+      else db(g)
+    }
 
-  def db: DB = ???
+    database = db + (g -> (studentNames :+ name))
+    database
+  }
 
-  def grade(g: Int): Seq[String] = ???
+  def db: DB = {
+    if (database.isEmpty) createDatabase()
+    database
+  }
 
-  def sorted: DB = ???
+  def grade(g: Int): Seq[String] = {
+    if (!db.contains(g)) Seq()
+    else db(g)
+  }
+
+  def sorted: DB = {
+    val sortedKeys = db.keys.toList.sorted
+    val sortedValues = sortedKeys.map(key => db(key).sorted)
+    val sortedDB = (sortedKeys zip sortedValues).toMap
+    sortedDB
+  }
+
+  private def createDatabase(): DB = {
+    database = Map()
+    database
+  }
 }
-
