107b24b85b19df2826488ef2e697f0107e8f583b
Sam Younger <syounger@usertesting.com>
2023-11-30 23:06:41 +0000

    Functional implementation

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,31 +1,47 @@
+import scala.annotation.tailrec
+
 object Bob {
-  def response(statement: String): String = {
-    val trimmedStatement = statement.trim
+  private type Reaction = (Boolean, String)
+  private val reactions =
+    Array[String => Reaction](
+      loudQuestion,
+      question,
+      shout,
+      noQuestion,
+      defaultResponse
+    )
 
-    if (trimmedStatement.isEmpty) {
-      return "Fine. Be that way!"
+  @tailrec
+  def response(statement: String, question: Int = 0): String = {
+    val reaction = reactions(question)(statement.trim)
+    if (reaction._1) {
+      return reaction._2
     }
 
-    if (shout(trimmedStatement) && question(trimmedStatement)) {
-      return "Calm down, I know what I'm doing!"
-    }
-
-    if (shout(trimmedStatement)) {
-      return "Whoa, chill out!"
-    }
-
-    if (question(trimmedStatement)) {
-      return "Sure."
-    }
-
-    "Whatever."
+    response(statement, question + 1)
   }
 
-  private def question(statement: String): Boolean = statement.endsWith("?")
+  private def loudQuestion(statement: String): Reaction =
+    (
+      shout(statement)._1 && question(statement)._1,
+      "Calm down, I know what I'm doing!"
+    )
 
-  private def shout(statement: String): Boolean =
-    hasLetters(statement) && statement.toUpperCase == statement
+  private def noQuestion(statement: String): Reaction =
+    (statement.isEmpty, "Fine. Be that way!")
+
+  private def question(statement: String): Reaction =
+    (statement.endsWith("?"), "Sure.")
+
+  private def shout(statement: String): Reaction =
+    (
+      hasLetters(statement) && statement.toUpperCase == statement,
+      "Whoa, chill out!"
+    )
 
   private def hasLetters(statement: String): Boolean =
     statement.exists(_.isLetter)
+
+  private def defaultResponse(statement: String): Reaction =
+    (true, "Whatever.")
 }
