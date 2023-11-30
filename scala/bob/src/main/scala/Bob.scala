b1297d153aee40aa04042579100c7257605949f5
Sam Younger <syounger@usertesting.com>
2023-11-30 19:28:15 +0000

    Working solution

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,3 +1,31 @@
 object Bob {
-  def response(statement: String): String = ???
+  def response(statement: String): String = {
+    val trimmedStatement = statement.trim
+
+    if (trimmedStatement.isEmpty) {
+      return "Fine. Be that way!"
+    }
+
+    if (shout(trimmedStatement) && question(trimmedStatement)) {
+      return "Calm down, I know what I'm doing!"
+    }
+
+    if (shout(trimmedStatement)) {
+      return "Whoa, chill out!"
+    }
+
+    if (question(trimmedStatement)) {
+      return "Sure."
+    }
+
+    "Whatever."
+  }
+
+  private def question(statement: String): Boolean = statement.endsWith("?")
+
+  private def shout(statement: String): Boolean =
+    hasLetters(statement) && statement.toUpperCase == statement
+
+  private def hasLetters(statement: String): Boolean =
+    statement.exists(_.isLetter)
 }
