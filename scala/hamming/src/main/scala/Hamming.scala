eac97146effb6d320e24177f9e143ac09772fda4
Sam Younger <syounger@usertesting.com>
2023-12-05 21:35:17 +0000

    Working solution

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,3 +1,19 @@
+import scala.annotation.tailrec
+
 object Hamming {
-  def distance(dnaStrandOne: String, dnaStrandTwo: String): Option[Int] = ???
+  @tailrec
+  def distance(dnaStrandOne: String,
+               dnaStrandTwo: String,
+               distance: Int = 0): Option[Int] = {
+    if (dnaStrandOne.length != dnaStrandTwo.length) {
+      return None
+    }
+
+    if (dnaStrandOne.isEmpty) {
+      return Some(distance)
+    }
+    val diffCount = if (dnaStrandOne.head == dnaStrandTwo.head) 0 else 1
+
+    Hamming.distance(dnaStrandOne.tail, dnaStrandTwo.tail, distance + diffCount)
+  }
 }
