71c5eaca41065a09af7551cb7173fa6dc65f50b0
samyounger <sam.younger@gmail.com>
2024-12-09 22:30:03 +0000

    Go -> welcome to tech palace solution

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,21 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	var border = strings.Repeat("*", numStarsPerLine)
+	return border + "\n" + welcomeMsg + "\n" + border
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	var msg = strings.ReplaceAll(oldMsg, "*", "")
+	var splitString = strings.ReplaceAll(msg, "\n", "")
+	return strings.TrimSpace(splitString)
 }
