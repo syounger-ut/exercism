cbc1b9a5ab4d0a3655963dc18c3bfb8d146e6830
samyounger <sam.younger@gmail.com>
2025-03-21 23:41:18 +0000

    Cards score

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -2,7 +2,38 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten":
+		return 10
+	case "jack":
+		return 10
+	case "queen":
+		return 10
+	case "king":
+		return 10
+	case "joker":
+		return 0
+	default:
+		return 0
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
