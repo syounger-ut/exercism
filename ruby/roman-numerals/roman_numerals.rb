471c365d1c672bdf859dc3a231aea61e75ff2cc0
Sam Younger <221829750+samyounger@users.noreply.github.com>
2025-12-06 09:43:38 +0000

    Numbers to strings

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,61 +1,25 @@
 require 'pry'
 
-class Fixnum
+class Integer
   ROMAN_NUMERALS = {
-    '1'   => 'I',
-    '5'   => 'V',
-    '10'  => 'X',
-    '50'  => 'L',
-    '100' => 'C',
+    '1000' => 'M',
     '500' => 'D',
-    '1000' => 'M'
+    '100' => 'C',
+    '50'  => 'L',
+    '10'  => 'X',
+    '5'   => 'V',
+    '1'   => 'I'
   }
 
   def to_roman
-    # Break the number into Thousands, Hundreds, Tens and Ones, and write down each in turn.
-    n = self
-    number_of_chars = n.to_s.chars.count
+    # How many do each of the roman numberals divide into the number
+    ROMAN_NUMERALS.map do |key, value|
+      divides = self / key.to_i
+      next if divides == 0
 
-    case number_of_chars
-    when 4
-      # code fo 1000's
-    when 3
-      # code for 100's
-    when 2
-      # code for 10's
-      # binding.pry
-      multiple = n.digits.last
-      tens = combine_numerals(multiple, 'X')
-      single = singles(n.digits.first)
-      [tens, single].join
-    when 1
-      # code for 1's
-      singles(n)
-    end
+      # if a single digit, need to use modulo
+      next 10 - (self % 10) if key == "I"
+      value
+    end.join
   end
-
-  private
-
-  def singles(n)
-    if n == 4
-      'IV'
-    elsif n == 5
-      'V'
-    elsif n == 6
-      'VI'
-    elsif n == 7
-      'VII'
-    elsif n == 8
-      'VIII'
-    elsif n == 9
-      'IX'
-    else
-      combine_numerals(n, 'I')
-    end
-  end
-
-  def combine_numerals(multiple, numeral)
-    multiple.times.map { |x| numeral }.join
-  end
-
 end
